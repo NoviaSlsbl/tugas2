623bbc7efee34b42e5d32383bc3cbe90837ea145
NoviaSlsbl <yulianilia2111@gmail.com>
2022-02-11 13:48:38 +0700

    commit pertama

---
+++ no1.js
@@ -0,0 +1,31 @@
+function minmax(novia){
+    const small =Math.min(...novia);
+    const big = Math.max(...novia);
+    console.log(small,big);
+}
+minmax([1,2,3,4,5]);
+minmax([1])
+minmax([22334455,6])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ no2.js
@@ -0,0 +1,34 @@
+function maxmin(array) {
+    const no = array.filter(no=>{
+        return no < 3
+    })
+    console.log(no);
+}
+maxmin([1, 2, 3]);
+
+
+function maxmin(array) {
+    const no = array.filter(no=>{
+        return no < 2
+    })
+    console.log(no);
+}
+maxmin([1, 2, 3]);
+
+function maxmin(array) {
+    const no = array.filter(no=>{
+        return no < 1
+    })
+    console.log(no);
+}
+maxmin([1, 2, 3]);
+
+function maxmin(array) {
+    const no = array.filter(no=>{
+        return no = 3
+    })
+    console.log(no);
+}
+maxmin([1, 2, 3],0);
+
+
